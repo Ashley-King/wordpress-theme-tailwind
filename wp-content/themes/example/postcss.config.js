9c303cafac9882ba56df834ba9b4d17599d44e54
Ashley-King <Ashley-King@users.noreply.github.com>
2023-04-11 07:49:54 -0400

    add postcss-header to preseerve stylesheet header and add a blank index file

--- wp-content/themes/example/postcss.config.js
+++ wp-content/themes/example/postcss.config.js
@@ -1,3 +1,5 @@
+const header = require('postcss-header');
+
 module.exports = {
   plugins: [
     require('tailwindcss'),
@@ -5,6 +7,18 @@ module.exports = {
     require('postcss-import'),
     require('cssnano')({
       preset: 'default',
+    }),
+    header({
+      header: `/*Theme Name: Example
+      Theme URI: https://giantly.co
+      Author: Ashley K
+      Author URI: https://giantly.co
+      Description: A custom WordPress theme.
+      Version: 1.0
+      License: GNU General Public License v2 or later
+      License URI: http://www.gnu.org/licenses/gpl-2.0.html
+      Text Domain: example
+*/`,
     }),
   ],
 }
